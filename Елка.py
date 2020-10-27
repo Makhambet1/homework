dc3d29c44a2aecf4cdc93c074edbd19b75d39e69
Makhambet1 <73330200+Makhambet1@users.noreply.github.com>
2020-10-27 14:37:47 +0600

    Add files via upload

---
+++ Елка.py
@@ -0,0 +1,8 @@
+ # Упражнение 1
+# print('Упражнение 1. Введите глубину ёлки:')
+#stars = int(input())
+print('Упражнение 1. Введите глубину ёлки:')
+stars = int(input())
+
+for star in range(1, stars+1):
+ print(star * '*')
