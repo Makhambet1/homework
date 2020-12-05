6e76c2eed71efd88d73d056e6048b58cce9c764f
Makhambet1 <73330200+Makhambet1@users.noreply.github.com>
2020-12-05 23:35:09 +0600

    Update pushpop.main.py

--- pushpop.main.py
+++ pushpop.main.py
@@ -1,25 +1,24 @@
-asd = []
+asd = []        
 zxc = []
-old = 0
-citizen = 0
+old = 0         
+citizen = 0         
 makha = input('Введите длину очереди: ')
 for a in range(int(makha)):
-    name = input('Выберите: 1.ветеран 2.обычный : ')
-    if (int(name) == 2):
+    name = input('Выберите: Мусульманин или христианин : ')
+    if (int(name) == 2):   
         citizen = citizen + 1
-    else:
+    else:     
         asd.insert(old, 1)
         old = old + 1
 
 
 for a in range(int(makha)):
-    if old == 0:
+    if old == 0:  
         for a in range(citizen):
-            print('обычный гражданин ')
-            zxc.append('обычный гражданин')
-        break
-    print('ветеран В.О.В ')
+            print('Православный христианин ')
+            zxc.append('Православный христианин')
+        break      
+    print('Правоверный мусульманин ')
     old = old - 1
-    zxc.append('ветеран В.О.В')
-
-print(zxc)
+    zxc.append('Правоверный мусульманин')   
+print(zxc)   
